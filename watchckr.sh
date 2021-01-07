8a8dadbd707b4268f4511030f9b0e69ef6e98839
John Yan <yan.jianhua@gmail.com>
2021-01-07 18:17:04 +0800

    Update watchckr.sh
    add timestamp, no wall

--- watchckr.sh
+++ watchckr.sh
@@ -1,24 +1,29 @@
-#!/bin/bash                                                                        
-                                                                                  
-while true; do                                                                    
-    ret1=`ps -aux | grep "store=data8 " | wc -l`                                  
-    ret2=`ps -aux | grep "store=data8a " | wc -l`                                 
-                                                                                  
-    wall "The Node8/8A status is $ret1, $ret2"                                             
-                                                                                  
-    if [ $ret1 = 1 ] || [ $ret2 = 1 ]; then                                       
-        /usr/sbin/ntpdate time.pool.aliyun.com                                    
-    fi                                                                            
-                                                                                  
-    if [ $ret1 = 1 ]; then                                                        
-       wall "Restart Node 8 ...."                                                 
-       runuser -l cockroach  -c "/opt/cockroach/start8.sh"                        
-    fi                                                                            
-                                                                                  
-    if [ $ret2 = 1 ]; then                                                        
-       wall "Restart Node 8A ...."                                                
-       runuser -l cockroach  -c "/opt/cockroach/start8a.sh"                       
-    fi                                                                            
-                                                                                  
-    sleep 90                                                                      
-done                                                                              
+!/bin/bash
+
+## start use nohup /opt/watchckr.sh > /opt/watch.log 2>&1 & 
+
+while true; do
+    ret1=`ps -aux | grep "store=data8 " | wc -l`
+    ret2=`ps -aux | grep "store=data8a " | wc -l`
+
+    echo "The Nodes status is $ret1, $ret2  "`date`
+
+    if [ $ret1 = 1 ] || [ $ret2 = 1 ]; then
+        /usr/sbin/ntpdate time.pool.aliyun.com
+    fi
+
+    if [ $ret1 = 1 ]; then
+       echo "Restart Node 8 ...."`date`
+       runuser -l cockroach  -c "/opt/cockroach/start8.sh"
+    fi
+
+    if [ $ret2 = 1 ]; then
+       echo "Restart Node 8A ...."`date`
+       runuser -l cockroach  -c "/opt/cockroach/start8a.sh"
+    fi
+
+    sleep 90
+done
+
+
+                                                                     
